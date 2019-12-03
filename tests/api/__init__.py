88b5aa5076654b27e30c51c3fc46201dc8c56bca
an5456 <527011764@qq.com>
2019-12-03 23:40:01 +0800

    i

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -1,73 +1,73 @@
-import re
-
-dolloar_regex_compile = re.compile(r"\$\$")
-# variable notation, e.g. ${var} or $var
-variable_regex_compile = re.compile(r"\$\{(\w+)\}|\$(\w+)")
-# function notation, e.g. ${func1($var_1, $var_3)}
-function_regex_compile = re.compile(r"\$\{(\w+)\(([\$\w\.\-/\s=,]*)\)\}")
-
-def is_var_or_func_exist(content):
-    """ check if variable or function exist
-    """
-    # if not isinstance(content, basestring):
-    #     return False
-
-    try:
-        match_start_position = content.index("$", 0)
-    except ValueError:
-        return False
-
-    while match_start_position < len(content):
-        dollar_match = dolloar_regex_compile.match(content, match_start_position)
-        if dollar_match:
-            match_start_position = dollar_match.end()
-            continue
-
-        func_match = function_regex_compile.match(content, match_start_position)
-        if func_match:
-            return True
-
-        var_match = variable_regex_compile.match(content, match_start_position)
-        if var_match:
-            print(var_match)
-            return True
-
-        return False
-
-
-# print(is_var_or_func_exist("jfdlsafjkl$code$test"))
-def regex_findall_variables(content):
-    """ extract all variable names from content, which is in format $variable
-
-    Args:
-        content (str): string content
-
-    Returns:
-        list: variables list extracted from string content
-
-    # Examples:
-    #     >>> regex_findall_variables("$variable")
-    #     ["variable"]
-    #
-    #     >>> regex_findall_variables("/blog/$postid")
-    #     ["postid"]
-    #
-    #     >>> regex_findall_variables("/$var1/$var2")
-    #     ["var1", "var2"]
-    #
-    #     >>> regex_findall_variables("abc")
-    #     []
-
-    """
-    try:
-        vars_list = []
-        for var_tuple in variable_regex_compile.findall(content):
-            vars_list.append(
-                var_tuple[0] or var_tuple[1]
-            )
-        return vars_list
-    except TypeError:
-        return []
-
-
-print(regex_findall_variables("fds${afd}sfdsfadsaf$tests$gggg")[2])
+# import re
+#
+# dolloar_regex_compile = re.compile(r"\$\$")
+# # variable notation, e.g. ${var} or $var
+# variable_regex_compile = re.compile(r"\$\{(\w+)\}|\$(\w+)")
+# # function notation, e.g. ${func1($var_1, $var_3)}
+# function_regex_compile = re.compile(r"\$\{(\w+)\(([\$\w\.\-/\s=,]*)\)\}")
+#
+# def is_var_or_func_exist(content):
+#     """ check if variable or function exist
+#     """
+#     # if not isinstance(content, basestring):
+#     #     return False
+#
+#     try:
+#         match_start_position = content.index("$", 0)
+#     except ValueError:
+#         return False
+#
+#     while match_start_position < len(content):
+#         dollar_match = dolloar_regex_compile.match(content, match_start_position)
+#         if dollar_match:
+#             match_start_position = dollar_match.end()
+#             continue
+#
+#         func_match = function_regex_compile.match(content, match_start_position)
+#         if func_match:
+#             return True
+#
+#         var_match = variable_regex_compile.match(content, match_start_position)
+#         if var_match:
+#             print(var_match)
+#             return True
+#
+#         return False
+#
+#
+# # print(is_var_or_func_exist("jfdlsafjkl$code$test"))
+# def regex_findall_variables(content):
+#     """ extract all variable names from content, which is in format $variable
+#
+#     Args:
+#         content (str): string content
+#
+#     Returns:
+#         list: variables list extracted from string content
+#
+#     # Examples:
+#     #     >>> regex_findall_variables("$variable")
+#     #     ["variable"]
+#     #
+#     #     >>> regex_findall_variables("/blog/$postid")
+#     #     ["postid"]
+#     #
+#     #     >>> regex_findall_variables("/$var1/$var2")
+#     #     ["var1", "var2"]
+#     #
+#     #     >>> regex_findall_variables("abc")
+#     #     []
+#
+#     """
+#     try:
+#         vars_list = []
+#         for var_tuple in variable_regex_compile.findall(content):
+#             vars_list.append(
+#                 var_tuple[0] or var_tuple[1]
+#             )
+#         return vars_list
+#     except TypeError:
+#         return []
+#
+#
+# print(regex_findall_variables("fds${afd}sfdsfadsaf$tests$gggg")[2])
