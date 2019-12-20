a1a47fe1c0ea5b69c7291394f5301bd14a25a1cc
an5456 <527011764@qq.com>
2019-12-21 02:57:40 +0800

    4w35

--- tests/main.py
+++ tests/main.py
@@ -3,14 +3,15 @@ import subprocess
 
 if __name__ == '__main__':
     # 获取系统类型，Mac还是Linux
-    os_type = subprocess.run("``uname", shell=True)
+
     subprocess.run("pytest -s --alluredir=./report;"
                    "allure generate ./report -o ./html --clean;"
                    , shell=True)
-    pid = subprocess.run("ps -ef|grep http.server | grep -E '[0-9] python3'|awk '{print $2}'", shell=True)
-    # pid = subprocess.run("ps -ef|grep python3|grep 8899|awk '{print $2}' |awk 'NR==2'", shell=True)
-    if pid is not "":
-        subprocess.run("kill -9" + str(pid) + "", shell=True)
-        print("kill report server ")
-    print("start report server")
-    subprocess.run("cd ./html;nohup python3 -m http.server 8899 >out.log 2>&1 & ", shell=True)
+    # pid = subprocess.run("ps -ef|grep http.server | grep -E '[0-9] python3'|awk '{print $2}'", shell=True)
+    # print(pid)
+    # # pid = subprocess.run("ps -ef|grep python3|grep 8899|awk '{print $2}' |awk 'NR==2'", shell=True)
+    # if pid is not "":
+    #     subprocess.run("kill -9" + str(pid) + "", shell=True)
+    #     print("kill report server ")
+    # print("start report server")
+    # subprocess.run("nohup python3 -m http.server 8899 >out.log 2>&1 & ", shell=True)
---
+++ tests/stat_report.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+killReport()
+{
+    pid=`ps -ef|grep http.server | grep -E '[0-9] python3'|awk '{print $2}'`
+    echo "report Id :$pid"
+    if [[ "$pid" = "" ]]
+    then
+        echo "no report pid alive"
+    else
+        kill -9 $pid
+    fi
+}
+killReport
+#
+#     if pid is not "":
+#         subprocess.run("kill -9" + str(pid) + "", shell=True)
+#         print("kill report server ")
+#     print("start report server")
+#     subprocess.run("nohup python3 -m http.server 8899 >out.log 2>&1 & ", shell=True)
