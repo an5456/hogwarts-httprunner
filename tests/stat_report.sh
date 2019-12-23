148c853033d229a36eca1431d23e60d85f3b341b
an5456 <39398302+an5456@users.noreply.github.com>
2019-12-23 22:03:56 +0800

    Update stat_report.sh

--- tests/stat_report.sh
+++ tests/stat_report.sh
@@ -9,23 +9,16 @@ killReport()
         echo "no report pid alive"
     else
         kill -9 $pid
-<<<<<<< HEAD
+
         echo "report server pid  kill"
-=======
+
         echo "start report server"
->>>>>>> 8c8f5de50eb2cf91b2de21f62168eb6cc26126fd
+
     fi
 }
 killReport
 cd ./html/
 echo "start report server"
 nohup python3 -m http.server 8899 >out.log 2>&1 &
-<<<<<<< HEAD
-=======
-#
-#     if pid is not "":
-#         subprocess.run("kill -9" + str(pid) + "", shell=True)
-#         print("kill report server ")
-#     print("start report server")
-#     subprocess.run("nohup python3 -m http.server 8899 >out.log 2>&1 & ", shell=True)
->>>>>>> 8c8f5de50eb2cf91b2de21f62168eb6cc26126fd
+
+
