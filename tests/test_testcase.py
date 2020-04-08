9dc74f7e65f420b51e44f603272bcf4e3eb6ffd4
an5456 <527011764@qq.com>
2020-04-08 08:41:28 +0800

    ty6931356

--- tests/test_testcase.py
+++ tests/test_testcase.py
@@ -19,22 +19,22 @@ class TestSingle:
         """斤斤计较测试"""
         Result.result_assert("testcase", "mubu_login.yml")
 
-    # @allure.story("测试2")
-    # def test_run_login(self):
-    #     """范德萨范德"""
-    #     Result.result_assert(filename="get_login.yml")
-    #
-    # @allure.story("测试登陆123")
-    # def test_get_login_submit(self):
-    #     """ 呵呵呵呵1234"""
-    #     Result.result_assert(filename="get_login_submit.yml")
+    @allure.story("测试2")
+    def test_run_login(self):
+        """范德萨范德"""
+        Result.result_assert(filename="get_login.yml")
 
-    # @allure.story("测试4")
-    # def test_get_home_page(self):
-    #     """测试呵呵呵呵"""
-    #     Result.result_assert(filename="get_home_page.yml")
-    #     allure.attach.file(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "login.csv"), "报告",
-    #                        allure.attachment_type.CSV)
+    @allure.story("测试登陆123")
+    def test_get_login_submit(self):
+        """ 呵呵呵呵1234"""
+        Result.result_assert(filename="get_login_submit.yml")
+
+    @allure.story("测试4")
+    def test_get_home_page(self):
+        """测试呵呵呵呵"""
+        Result.result_assert(filename="get_home_page.yml")
+        allure.attach.file(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "login.csv"), "报告",
+                           allure.attachment_type.CSV)
 
     @allure.story("上传文件")
     def test_upfile(self):
