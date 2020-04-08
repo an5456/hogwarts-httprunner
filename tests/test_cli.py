8539a407e261cad2dcf01a0ff0221aa6e6688d26
an5456 <527011764@qq.com>
2020-04-08 15:49:27 +0800

    yj

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-# import os
-# import unittest
-# import subprocess
-#
-#
-# class TestCli(unittest.TestCase):
-#     def test_hogrun_single_yaml(self):
-#         single_api_yaml = os.path.join(os.path.dirname(__file__), "api", "get_login_submit.yml")
-#         subprocess.run("python -m core.cli {}".format(single_api_yaml))
