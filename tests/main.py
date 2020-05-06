81db3b7b0cb3f2e0f0b78705cbf4afcced10c635
an5456 <527011764@qq.com>
2020-05-06 10:37:37 +0800

    ii

--- tests/main.py
+++ tests/main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     subprocess.run(
         "rm -rf report;"
         "rm -rf html;"
-        "pytest -s --alluredir=./report;"           
-        "allure generate ./report -o ./html --clean;", shell=True)
+        "pytest -s --alluredir=report;"           
+        "allure generate report -o html --clean;", shell=True)
